e5a0f5349275a44818c8c8ba02656be570cb8d3a
Stephania Rincon Godinez <srincon3@ejhu.com>
2023-12-04 05:32:07 -0500

    wage gap

--- script.js
+++ script.js
@@ -72,67 +72,67 @@ Promise.all([
 });
 
 
-// wage gap
-const width = 960;
-const height = 600;
+// // wage gap
+// const width = 960;
+// const height = 600;
 
-// Set up the x and y scales
+// // Set up the x and y scales
 
-const x = d3.scaleTime()
-  .range([0, width]);
+// const x = d3.scaleTime()
+//   .range([0, width]);
 
-const y = d3.scaleLinear()
-  .range([height, 0]);
+// const y = d3.scaleLinear()
+//   .range([height, 0]);
 
-// Create the SVG element and append it to the chart container
+// // Create the SVG element and append it to the chart container
 
-const svg_wg = d3.select("pay-gap-svg")
+// const svg_wg = d3.select("pay-gap-svg")
 
-// Create a fake dataset
-const dataset = [
-  { date: new Date("2022-01-01"), value: 200 },
-  { date: new Date("2022-02-01"), value: 250 },
-  { date: new Date("2022-03-01"), value: 180 },
-  { date: new Date("2022-04-01"), value: 300 },
-  { date: new Date("2022-05-01"), value: 280 },
-  { date: new Date("2022-06-01"), value: 220 },
-  { date: new Date("2022-07-01"), value: 300 },
-  { date: new Date("2022-08-01"), value: 450 },
-  { date: new Date("2022-09-01"), value: 280 },
-  { date: new Date("2022-10-01"), value: 600 },
-  { date: new Date("2022-11-01"), value: 780 },
-  { date: new Date("2022-12-01"), value: 320 }
-];
+// // Create a fake dataset
+// const dataset = [
+//   { date: new Date("2022-01-01"), value: 200 },
+//   { date: new Date("2022-02-01"), value: 250 },
+//   { date: new Date("2022-03-01"), value: 180 },
+//   { date: new Date("2022-04-01"), value: 300 },
+//   { date: new Date("2022-05-01"), value: 280 },
+//   { date: new Date("2022-06-01"), value: 220 },
+//   { date: new Date("2022-07-01"), value: 300 },
+//   { date: new Date("2022-08-01"), value: 450 },
+//   { date: new Date("2022-09-01"), value: 280 },
+//   { date: new Date("2022-10-01"), value: 600 },
+//   { date: new Date("2022-11-01"), value: 780 },
+//   { date: new Date("2022-12-01"), value: 320 }
+// ];
 
-// Define the x and y domains
-x.domain(d3.extent(dataset, d => d.date));
-y.domain([0, d3.max(dataset, d => d.value)]);
+// // Define the x and y domains
+// x.domain(d3.extent(dataset, d => d.date));
+// y.domain([0, d3.max(dataset, d => d.value)]);
 
-// Add the x-axis
+// // Add the x-axis
 
-svg_wg.append("g")
-  .attr("transform", `translate(0,${height})`)
-  .call(d3.axisBottom(x)
-    .ticks(d3.timeMonth.every(1)) 
-    .tickFormat(d3.timeFormat("%b %Y"))); 
+// svg_wg.append("g")
+//   .attr("transform", `translate(0,${height})`)
+//   .call(d3.axisBottom(x)
+//     .ticks(d3.timeMonth.every(1)) 
+//     .tickFormat(d3.timeFormat("%b %Y"))); 
 
 
-// Add the y-axis
+// // Add the y-axis
 
-svg_wg.append("g")
-  .call(d3.axisLeft(y))
+// svg_wg.append("g")
+//   .call(d3.axisLeft(y))
 
-// Create the line generator
+// // Create the line generator
 
-const line = d3.line()
-  .x(d => x(d.date))
-  .y(d => y(d.value));
+// const line = d3.line()
+//   .x(d => x(d.date))
+//   .y(d => y(d.value));
 
-// Add the line path to the SVG element
+// // Add the line path to the SVG element
 
-svg_wg.append("path")
-  .datum(dataset)
-  .attr("fill", "none")
-  .attr("stroke", "steelblue")
-  .attr("stroke-width", 1)
-  .attr("d", line);
+// svg_wg.append("path")
+//   .datum(dataset)
+//   .attr("fill", "none")
+//   .attr("stroke", "steelblue")
+//   .attr("stroke-width", 1)
+//   .attr("d", line);
